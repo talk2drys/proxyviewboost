937aca35250aa0e1e5d11f620164977b02e8445c
Daniel Temiagin <talk2drys@gmail.com>
2023-05-10 14:39:48 -0600

    use logging

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import sys
-# from play_youtube import VirtualHuman
-# from custom_thread import CustomThread
 from sock import SSHSock5Proxy
 from util import load_sock_servers, run_socks
 import asyncio
@@ -8,6 +6,8 @@ from error import Error
 import toml
 from result import Result, Ok, Err
 from typing import Any, List
+import logging
+
 
 if __name__ == "__main__":
     # load configuration file
@@ -16,20 +16,20 @@ if __name__ == "__main__":
     TIMEOUT_IN_SECS: int = config['timeout']
     NUMBER_OF_PARALLEL: int = config['number_of_parallel']
 
-    print("STARTING IROBOT!!!")
-    print("Loading sock server file")
+    logging.info("STARTING IROBOT!!!")
+    logging.info("Loading sock server file")
     loaded_sock_servers: Result[List[SSHSock5Proxy], Error] = load_sock_servers("settings/servers.txt")
 
     match loaded_sock_servers:
         case Ok(value):
             sock_servers = value
         case Err(err):
-            print("Error loading or parsing proxy file")
+            logging.error("Error loading or parsing proxy file")
             sys.exit(2)
 
     loop = asyncio.get_event_loop()
     for i in range(0, len(sock_servers), 5):
         loop.run_until_complete(run_socks(sock_servers[i:i+NUMBER_OF_PARALLEL]))
-        print(f"Completed {i+NUMBER_OF_PARALLEL} proxies")
+        logging.info(f"Completed {i+NUMBER_OF_PARALLEL} proxies")
 
-    print("Exiting")
+    logging.info("Exiting")
